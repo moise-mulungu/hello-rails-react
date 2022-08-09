819be24b74f26cb1f563eec84b879f02f89cbb5e
moise <moisemlg90@gmail.com>
2022-08-09 17:12:51 +0200

    Setup the requirements

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,9 +1,9 @@
-class Api.v1::GreetingsController < ApplicationController
+class Api::V1::GreetingsController < ApplicationController
   # GET /greetings
   # GET /greetings.json
   def index
-    @greetings = Greeting.all.order(Random.rand(Greeting.count)).first
-    render json: @greetings
+    @greetings = Greeting.all
+    render json: @greetings[rand(@greetings.length - 1)]
   end
 
   #   # GET /greetings/1
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1, defaults: { format: 'json' } do
-      get 'messages', to: 'greeting#index'
+      get 'messages', to: 'greetings#index'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
