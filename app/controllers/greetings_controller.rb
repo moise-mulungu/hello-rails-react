d785410f74a087f9d12b703bb8f6f579c4c81f81
moise <moisemlg90@gmail.com>
2022-08-09 09:49:20 +0200

    Install react

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,10 +1,4 @@
 class GreetingsController < ApplicationController
   def hello
-    render params[:formal :json => { :message => "Good morning!" }]
-    render params[:informal :json => { :message => "Hi!" }] 
-    render params[:welcome :json => { :message => "Nice to meet you!" }]
-    render params[:farewell :json => { :message => "Goodbye!" }]
-    render params[:popular :json => { :message => "Hello!" }]
-
   end
 end
