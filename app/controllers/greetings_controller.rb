ec0553fe8b650d8dc5d9e04a1225f514f8e093a1
moise <moisemlg90@gmail.com>
2022-08-09 07:07:27 +0200

    Add five messages in the hello action

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,10 @@
 class GreetingsController < ApplicationController
   def hello
-    render json: {hello: 'world'}
-    
+    render params[:formal :json => { :message => "Good morning!" }]
+    render params[:informal :json => { :message => "Hi!" }] 
+    render params[:welcome :json => { :message => "Nice to meet you!" }]
+    render params[:farewell :json => { :message => "Goodbye!" }]
+    render params[:popular :json => { :message => "Hello!" }]
+
   end
 end
